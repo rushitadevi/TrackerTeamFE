411c0d3294e12349110b8567f9ca2b7444a9f0fc
rob <robertahuber88@gmail.com>
2020-01-14 19:06:48 +0100

    changed around code to display data according to url state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,18 +4,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from 'react-redux';
 import StudentDashboard from "./StudentDashboard";
 import ManagerDashboard from "./ManagerDashboard";
-import FilteredSearch from "./FilteredSearch";
+import FilteredSearchBar from "./FilteredSearchBar";
 import Navbar from "./Navbar";
 import configureStore from "../store";
-import { ok } from "assert";
+// import { ok } from "assert";
 
 
 class Main extends Component {
   state = {}
-  constructor(props) {
-    super(props);
-
-  }
 
   render() {
     return (
@@ -24,7 +20,7 @@ class Main extends Component {
         <Navbar/>
         <Container fluid className="studentHomepage">
      
-          <FilteredSearch/>
+          {/* <FilteredSearchBar/> */}
           {/* <StudentDashboard triggerSearch={this.search} location={this.state.locationResults} /> */}
           <Route path="/student" exact component={StudentDashboard} />
           </Container>
--- src/components/StudentDashboard.jsx
+++ src/components/StudentDashboard.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
-import NavBar from "./Navbar";
 // import { Link } from "react-router-dom"
-import { Container, Input, Card, CardHeader, CardBody } from "reactstrap";
-import {
-  getJobCategory,
-  getSearch,
-  // getLocation
-} from "../Actions/apiFetches";
+import { Container, Card, CardHeader, CardBody, Input} from "reactstrap";
+import {getJobCategory,  getSearch} from "../Actions/apiFetches";
 
 const mapStateToProps = state => state;
 
 const mapDispatchToProps = dispatch => ({
   getJobCategoryThunk: () => dispatch(getJobCategory()),
-  getSearchThunk: () => dispatch(getSearch()),
-  // getLocationThunk: () => dispatch(getLocation())
+  getSearchThunk: (url) => dispatch(getSearch(url)),
 });
-
 
 
 class StudentDashboard extends React.Component {
 
   state = {
     showModal: false,
+    company: "",
+    role: "",
+    level: "senior",
+    location: "",
+    url: null
   };
 
   componentDidMount = async () => {
@@ -32,12 +30,98 @@ class StudentDashboard extends React.Component {
     console.log(this.props.publicAPIfetches.jobCategory);
   };
 
+  searchInput = async (value) => {
+    value.preventDefault();
+
+    // debugger;
+
+if(!this.state.url ){
+let url = "search=";
+
+    if (this.state.company.length >= 4)
+        url += "+" + this.state.company
+    
+    if (this.state.role.length >= 4)
+        url += "+" + this.state.role
+    
+    if (this.state.level.length) 
+        url += "+" + this.state.level
+    
+    if (this.state.location.length >= 4) 
+        url += "&location=" + this.state.location
+    
+    // else{
+    //     await this.props.getSearchThunk("");
+    // }
+    console.log(url)
+
+    this.setState({
+        url: url
+    })
+    console.log(this.state.url)
+    await this.props.getSearchThunk(url);
+} else{
+    var url = null
+    this.setState({
+        url: url
+    })
+}
+    //if fetch is an empty array return a message: (no results matching your search)
+}
   toggleModal = () => {
     this.setState({ showModal: !this.state.showModal });
   };
 
   render() {
     return (
+      <>
+      <Container className="filterBar">
+      <h5 id="logo">TrackeR</h5>
+      <hr
+          style={{
+              backgroundColor: "#fcffff",
+              height: 0.2,
+              marginTop: "93px"
+          }}
+      />
+
+      <i className="material-icons" id="searchIcon"> search</i>
+      <Input className="searchCompany"
+          id="search"
+          placeholder="Company"
+          value={this.state.company || ''}
+          onChange={(e) => this.setState({ company: e.currentTarget.value })} />
+
+      <i className="material-icons" id="roleIcon"> work_outline</i>
+      <Input id="jobRole"
+          placeholder="Role"
+          value={this.state.role || ''}
+          onChange={(e) => this.setState({ role: e.currentTarget.value })} />
+
+      <i className="material-icons" id="entryLevelIcon"> file_copy</i>
+      <select className="form-control" id="entryLevel" value={this.state.level || 'senior'} onChange={(e) => this.setState({ level: e.currentTarget.value })}>
+          {/* <option selected="selected"> Entry Level </option> */}
+          <option value="senior">Senior</option>
+          <option value="junior">Junior</option>
+      </select>
+      <img
+          className="locationImg"
+          src="https://www.freeiconspng.com/uploads/simple-location-icon-png-22.png"
+          alt="/"
+      />
+
+      <Input
+          className="location"
+          id="location"
+          placeholder="Location"
+          value={this.state.location || ''}
+          onChange={(e) => this.setState({ location: e.currentTarget.value })}
+      />
+
+      <button className="searchButton" type='submit' onClick={this.searchInput}>Search</button>
+
+  </Container>
+   {!this.state.url && (
         <Container className="dashboardMainDisplay">
           <div>
             <Card id="wishList">
@@ -95,8 +179,16 @@ class StudentDashboard extends React.Component {
             </Card>
           </div>
         </Container>
-
-
+   )}
+      {this.state.url && (
+          <Container className="dashboardMainDisplay">
+          {this.props.publicAPIfetches.filteredSearch && this.props.publicAPIfetches.filteredSearch.map((jobs, index) => (                                         
+                             <div>{jobs.company} / {jobs.title}</div>
+                             )
+                             )}
+          </Container>
+      )}
+</>
     );
   }
 }
